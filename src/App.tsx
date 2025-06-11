7910c13990b562817805763c2c410c05f93c71e5
anish-inti <124215693+anish-inti@users.noreply.github.com>
2025-06-11 14:29:24 +0530

    Add dynamic route for place details

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -31,6 +30,7 @@ const App = () => (
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/places" element={<Places />} />
+              <Route path="/places/:id" element={<Places />} />
               <Route path="/search" element={<Search />} />
               <Route path="/favorites" element={<Favorites />} />
               <Route path="/profile" element={<Profile />} />
